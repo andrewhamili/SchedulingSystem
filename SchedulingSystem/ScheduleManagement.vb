0437b0fe215e45f205bc034dbd23d7507240b015
andrewhamili <rockr1521@gmail.com>
2016-12-30 13:38:09 +0800

    Edit / Delete Subjects Windows Development ongoing.

---
+++ SchedulingSystem/ScheduleManagement.vb
@@ -0,0 +1,7 @@
+﻿Public Class ScheduleManagement
+
+    
+    Private Sub ScheduleManagement_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
