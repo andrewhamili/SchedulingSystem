bb8101f0e3c98b953aaff4f83531a81ede4a69d7
andrewhamili <rockr1521@gmail.com>
2017-01-28 19:29:57 +0800

    Prepare

--- SchedulingSystem/AvailableRooms.Designer.vb
+++ SchedulingSystem/AvailableRooms.Designer.vb
@@ -27,34 +27,36 @@ Partial Class AvailableRooms
         Me.lblName = New System.Windows.Forms.Label()
         Me.lbl_avRoom = New System.Windows.Forms.Label()
         Me.lbl_user = New System.Windows.Forms.Label()
-        CType(Me.DataGridAvailableRooms,System.ComponentModel.ISupportInitialize).BeginInit
-        Me.SuspendLayout
+        CType(Me.DataGridAvailableRooms, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
         '
         'DataGridAvailableRooms
         '
-        Me.DataGridAvailableRooms.AllowUserToAddRows = false
-        Me.DataGridAvailableRooms.AllowUserToDeleteRows = false
-        Me.DataGridAvailableRooms.AllowUserToResizeRows = false
+        Me.DataGridAvailableRooms.AllowUserToAddRows = False
+        Me.DataGridAvailableRooms.AllowUserToDeleteRows = False
+        Me.DataGridAvailableRooms.AllowUserToResizeRows = False
         Me.DataGridAvailableRooms.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
         Me.DataGridAvailableRooms.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridAvailableRooms.Dock = System.Windows.Forms.DockStyle.Bottom
         Me.DataGridAvailableRooms.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically
-        Me.DataGridAvailableRooms.Location = New System.Drawing.Point(0, 140)
+        Me.DataGridAvailableRooms.Location = New System.Drawing.Point(0, 119)
+        Me.DataGridAvailableRooms.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.DataGridAvailableRooms.Name = "DataGridAvailableRooms"
-        Me.DataGridAvailableRooms.ReadOnly = true
-        Me.DataGridAvailableRooms.RowHeadersVisible = false
+        Me.DataGridAvailableRooms.ReadOnly = True
+        Me.DataGridAvailableRooms.RowHeadersVisible = False
         Me.DataGridAvailableRooms.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
-        Me.DataGridAvailableRooms.Size = New System.Drawing.Size(1272, 450)
+        Me.DataGridAvailableRooms.Size = New System.Drawing.Size(1362, 623)
         Me.DataGridAvailableRooms.TabIndex = 3
         '
         'lblName
         '
-        Me.lblName.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom)  _
-            Or System.Windows.Forms.AnchorStyles.Left),System.Windows.Forms.AnchorStyles)
-        Me.lblName.AutoSize = true
-        Me.lblName.Font = New System.Drawing.Font("Arial", 12!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.lblName.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lblName.AutoSize = True
+        Me.lblName.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lblName.ForeColor = System.Drawing.Color.Green
-        Me.lblName.Location = New System.Drawing.Point(11, 12)
+        Me.lblName.Location = New System.Drawing.Point(16, 17)
+        Me.lblName.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblName.Name = "lblName"
         Me.lblName.Size = New System.Drawing.Size(114, 19)
         Me.lblName.TabIndex = 4
@@ -63,9 +65,10 @@ Partial Class AvailableRooms
         'lbl_avRoom
         '
         Me.lbl_avRoom.Anchor = System.Windows.Forms.AnchorStyles.None
-        Me.lbl_avRoom.AutoSize = true
-        Me.lbl_avRoom.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
-        Me.lbl_avRoom.Location = New System.Drawing.Point(495, 69)
+        Me.lbl_avRoom.AutoSize = True
+        Me.lbl_avRoom.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_avRoom.Location = New System.Drawing.Point(742, 96)
+        Me.lbl_avRoom.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lbl_avRoom.Name = "lbl_avRoom"
         Me.lbl_avRoom.Size = New System.Drawing.Size(179, 24)
         Me.lbl_avRoom.TabIndex = 5
@@ -73,36 +76,38 @@ Partial Class AvailableRooms
         '
         'lbl_user
         '
-        Me.lbl_user.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom)  _
-            Or System.Windows.Forms.AnchorStyles.Left),System.Windows.Forms.AnchorStyles)
-        Me.lbl_user.AutoSize = true
-        Me.lbl_user.Font = New System.Drawing.Font("Arial", 12!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.lbl_user.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lbl_user.AutoSize = True
+        Me.lbl_user.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lbl_user.ForeColor = System.Drawing.Color.Green
-        Me.lbl_user.Location = New System.Drawing.Point(121, 13)
+        Me.lbl_user.Location = New System.Drawing.Point(138, 17)
+        Me.lbl_user.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lbl_user.Name = "lbl_user"
-        Me.lbl_user.Size = New System.Drawing.Size(176, 19)
+        Me.lbl_user.Size = New System.Drawing.Size(0, 19)
         Me.lbl_user.TabIndex = 6
-        Me.lbl_user.Text = "Batallones, Charish A."
         '
         'AvailableRooms
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255,Byte),Integer), CType(CType(192,Byte),Integer), CType(CType(192,Byte),Integer))
-        Me.ClientSize = New System.Drawing.Size(1272, 590)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1362, 742)
         Me.Controls.Add(Me.lbl_user)
         Me.Controls.Add(Me.lbl_avRoom)
         Me.Controls.Add(Me.lblName)
         Me.Controls.Add(Me.DataGridAvailableRooms)
-        Me.Icon = CType(resources.GetObject("$this.Icon"),System.Drawing.Icon)
+        Me.Font = New System.Drawing.Font("Arial", 12.0!)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "AvailableRooms"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Available Rooms"
-        CType(Me.DataGridAvailableRooms,System.ComponentModel.ISupportInitialize).EndInit
-        Me.ResumeLayout(false)
-        Me.PerformLayout
+        CType(Me.DataGridAvailableRooms, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
 
-End Sub
+    End Sub
 
     Friend WithEvents DataGridAvailableRooms As DataGridView
     Friend WithEvents lblName As Label
